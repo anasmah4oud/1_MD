16864eb92c7173eee0070c979e38510579d13cd1
aclassnumberonee-hash <aclassnumberonee@gmail.com>
2026-07-02 20:18:13 +0300

    Initial commit

--- server.ts
+++ server.ts
@@ -13,6 +13,12 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// Simple request logger for debugging API routing
+app.use((req, res, next) => {
+  console.log(`[HTTP] ${req.method} ${req.url}`);
+  next();
+});
+
 // --- Interfaces ---
 interface Group {
   id: string;
